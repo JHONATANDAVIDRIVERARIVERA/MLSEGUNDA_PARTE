de808b60e9b7c9b9b78f66951eaf2a84a0885de3
JHONATANDAVIDRIVERARIVERA <jhonatandavidr3@gmail.com>
2025-10-11 07:59:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-import Naivebayes
+import NaiveBayes
 from flask import Flask, render_template, request
 import matplotlib
 matplotlib.use('Agg')
